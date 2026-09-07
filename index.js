e2df054ec63a6fc909d0df3ff37ff372f537da14
ghilsh <129766569+ghilsh@users.noreply.github.com>
2026-09-07 02:20:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -75,24 +75,24 @@ async function reset_dictionary() {
   messages_with_attachments = {};
 }
 
-async function scambot_ownage(message,author_id) { // checks if user has sent three messages with attachments within ten seconds. if so, it kicks them and deletes all their messages with attachments from the last fifteen secs
-  console.log('reached scambot ownage funciton');
-  if (message.attachments.size > 0) {
-      messages_with_attachments[author_id] = messages_with_attachments[author_id].unshift(message);
-      if (messages_with_attachments[author_id].length < 3){
-        return;
-      } 
-      var third_to_last = messages_with_attachments[author_id][2]
-      if ((message.createdTimestamp - third_to_last.createdTimestamp) < 10000.0) {
-        await message.author.kick()
-        for (const mess of messages_with_attachments[author_id]) {
-          if ((message.createdTimestamp - mess.createdTimestamp) < 15000.0) {
-            await mess.delete();
-          }
-        }
-      }
-    }
-};
+//async function scambot_ownage(message,author_id) { // checks if user has sent three messages with attachments within ten seconds. if so, it kicks them and deletes all their messages with attachments from the last fifteen secs
+//  console.log('reached scambot ownage funciton');
+//  if (message.attachments.size > 0) {
+//      messages_with_attachments[author_id] = messages_with_attachments[author_id].unshift(message);
+//      if (messages_with_attachments[author_id].length < 3){
+//        return;
+//      } 
+//      var third_to_last = messages_with_attachments[author_id][2]
+//      if ((message.createdTimestamp - third_to_last.createdTimestamp) < 10000.0) {
+//        await message.author.kick()
+//        for (const mess of messages_with_attachments[author_id]) {
+//          if ((message.createdTimestamp - mess.createdTimestamp) < 15000.0) {
+//            await mess.delete();
+//          }
+//        }
+//      }
+//    }
+//};
 
 bot.on('messageCreate', async message => {
     scambot_ownage(message,message.author.id);
@@ -130,6 +130,21 @@ bot.on('messageCreate', async message => {
             console.error(error);
         }
         return;
+    } // SCAM BOT OWNAGE - MUST BE AT THE END
+    if (message.attachments.size > 0) {
+      messages_with_attachments[message.author.id] = messages_with_attachments[message.author.id].unshift(message);
+      if (messages_with_attachments[message.author.id].length < 3){
+        return;
+      } 
+      var third_to_last = messages_with_attachments[message.author.id][2]
+      if ((message.createdTimestamp - third_to_last.createdTimestamp) < 10000.0) {
+        await message.author.kick()
+        for (const mess of messages_with_attachments[message.author.id]) {
+          if ((message.createdTimestamp - mess.createdTimestamp) < 15000.0) {
+            await mess.delete();
+          }
+        }
+      }
     }
 });
 bot.login(process.env.TOKEN);
