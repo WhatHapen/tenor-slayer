817be1d520cc5d7c480e585a0b7b64d2bb4c0d5e
ghilsh <129766569+ghilsh@users.noreply.github.com>
2026-09-07 01:41:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,8 @@ async function reset_dictionary() {
 async function scambot_ownage(message) { // checks if user has sent three messages with attachments within ten seconds. if so, it kicks them and deletes all their messages with attachments from the last fifteen secs
   console.log('reached scambot ownage funciton');
   if (message.attachments.size > 0) {
-      messages_with_attachments[message.author.id].unshift(message)
+      console.log(messages_with_attachments);
+      messages_with_attachments[message.author.id].unshift(message);
       if (messages_with_attachments[message.author.id].length < 3){
         return;
       } 
